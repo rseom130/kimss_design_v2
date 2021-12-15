53272270a85053a13b874567ffc9ba6723085555
kimss <rseom130@gmail.com>
2021-12-16 00:42:58 +0900

    레이아웃 변경

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function f_theme_change() {
+    event.preventDefault();
+
+    var src = '';
+
     if ($('body').hasClass('dark')) {
         $('body').removeClass('dark');
         $('#theme_var').attr('href', './css/var_light.css');
+
+        $('.theme-icon').attr('src', './resource/sun.svg');
+        $('.theme-icon').attr('height', 24);
+
+        $('.custom-change-icon').each(function () {
+            src = $(this).attr('src');
+            $(this).attr('src', src.replace('_dark.svg', '.svg'));
+        });
     } else {
         $('body').addClass('dark');
         $('#theme_var').attr('href', './css/var_dark.css');
+
+        $('.theme-icon').attr('src', './resource/moon.svg');
+        $('.theme-icon').attr('height', 22);
+
+        $('.custom-change-icon').each(function () {
+            src = $(this).attr('src');
+            $(this).attr('src', src.replace('.svg', '_dark.svg'));
+        });
     }
 }
